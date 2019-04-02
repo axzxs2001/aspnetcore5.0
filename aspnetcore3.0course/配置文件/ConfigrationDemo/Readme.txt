12503c77de02ddd690fc93029eb0fda472c00d84
桂素伟 <axzxs2001@163.com>
2019-04-02 17:15:12 +0900

    配置文件案例完成。

---
+++ aspnetcore3.0course/配置文件/ConfigrationDemo/Readme.txt
@@ -0,0 +1,18 @@
+﻿一、机密文件只能在开发环境下使用
+1、.csproj文件的<PropertyGroup></PropertyGroup>节点中添加
+  <UserSecretsId>d7a40274-87df-43ba-999b-d8719940b7a7</UserSecretsId>
+2、操作机密文件
+   a、设置机密文件  dotnet user-secrets set "dbpassword" "123456" --project ".csproj所在目录"
+   b、设置多个机密文件  type .\input.json | dotnet user-secrets set --project ".csproj所在目录"
+   c、查看机密文件  dotnet user-secrets list --project ".csproj所在目录"
+   d、删除机密文件  dotnet user-secrets remove "dbpassword"  --project ".csproj所在目录"
+   e、删除所有机密文件 dotnet user-secrets clear --project ".csproj所在目录"
+3、ConfigureServices中读取
+   var password = Configuration["dbpassword"];
+
+二、环境（Development,Staging,Production）
+	在运行前设置ASPNETCORE_ENVIRONMENT环境变量为Development,Staging,Production
+	
+	在开发环境中，项目“属性”-“调试”里设置ASPNETCORE_ENVIRONMENT为Development
+	Jenkins发布Docker状况下，会在dockefile设置环境变量为Staging
+	Linux环境下：Environment=ASPNETCORE_ENVIRONMENT=Production
---
+++ aspnetcore3.0course/配置文件/ConfigrationDemo/Startup.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace ConfigrationDemo
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+      
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var password = Configuration["dbpassword"];
+            Console.WriteLine(password);
+
+            var appsetting = new Appsetting();
+            Configuration.GetSection("Appsetting").Bind(appsetting);
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+        }
+
+  
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc();
+        }
+    }
+}
