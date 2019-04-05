dfcf4ba66ccab55fd115fb78af652b6c289d6932
桂素伟 <axzxs2001@163.com>
2019-04-05 12:27:03 +0900

    固定角色Demo

---
+++ aspnetcore3.0course/验证和授权/AuthenticationAuthorization_Cookie_01/Controllers/HomeController.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using AuthenticationAuthorization_Cookie_01.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using System.Security.Claims;
+
+namespace AuthenticationAuthorization_Cookie_01.Controllers
+{
+    [Authorize(Roles = "admin,system")]
+    public class HomeController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [AllowAnonymous]
+        [HttpGet("login")]
+        public IActionResult Login(string returnUrl = null)
+        {
+            TempData["returnUrl"] = returnUrl;
+            return View();
+        }
+        [AllowAnonymous]
+        [HttpPost("login")]
+        public async Task<IActionResult> LoginAsync(string userName, string password, string returnUrl = null)
+        {
+            var list = new List<dynamic> {
+                 new { UserName = "gsw", Password = "111111", Role = "admin",Name="桂素伟" },
+                 new { UserName = "aaa", Password = "222222", Role = "system" ,Name="路人甲"}
+             };
+            var user = list.SingleOrDefault(s => s.UserName == userName && s.Password == password);
+            if (user != null)
+            {
+                //用户标识
+                var identity = new ClaimsIdentity(CookieAuthenticationDefaults.AuthenticationScheme);
+                identity.AddClaim(new Claim(ClaimTypes.Sid, userName));
+                identity.AddClaim(new Claim(ClaimTypes.Name, user.Name));
+                identity.AddClaim(new Claim(ClaimTypes.Role, user.Role));
+                await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(identity));
+                if (returnUrl == null)
+                {
+                    returnUrl = TempData["returnUrl"]?.ToString();
+                }
+                if (returnUrl != null)
+                {
+                    return Redirect(returnUrl);
+                }
+                else
+                {
+                    return Redirect("/");
+                }
+            }
+            else
+            {
+                return BadRequest("用户名或密码错误！");
+            }       
+        }
+    
+        public async Task<IActionResult> Logout()
+        {
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            return RedirectToAction("Index", "Home");
+        }
+        [AllowAnonymous]
+        [HttpGet("denied")]
+        public IActionResult Denied()
+        {
+            return View();
+        }
+        [Authorize(Roles = "admin")]
+        public IActionResult AdminPage()
+        {
+            return View();
+        }
+        [Authorize(Roles = "system")]
+        public IActionResult SystemPage()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+        [AllowAnonymous]
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+    }
+}
---
+++ aspnetcore3.0course/验证和授权/AuthenticationAuthorization_Cookie_01/Startup.cs
@@ -0,0 +1,76 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace AuthenticationAuthorization_Cookie_01
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+
+
+            services.AddMvc()
+                .AddNewtonsoftJson();
+
+            //添加认证Cookie信息
+            services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+                .AddCookie(options =>
+                {
+                    options.LoginPath = new PathString("/login");
+                    options.AccessDeniedPath = new PathString("/denied");
+                });
+        }
+
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+            app.UseAuthentication();
+            app.UseRouting(routes =>
+            {
+                routes.MapControllerRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+                routes.MapRazorPages();
+            });
+
+            app.UseCookiePolicy();
+         
+            app.UseAuthorization();
+
+        }
+    }
+}
