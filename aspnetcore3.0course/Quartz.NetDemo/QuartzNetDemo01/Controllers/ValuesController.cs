bd14fdb5de1834b6650835b82908de3ebb140051
桂素伟 <axzxs2001@163.com>
2019-04-16 17:58:44 +0900

    Quartz案例

---
+++ aspnetcore3.0course/Quartz.NetDemo/QuartzNetDemo01/Controllers/ValuesController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Quartz;
+using System.IO;
+using Microsoft.Extensions.FileProviders;
+using QuartzNetDemo01.Model.DataModel;
+using QuartzNetDemo01.Model;
+
+namespace QuartzNetDemo01.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        IBackgroundRepository _backgroundRepository;
+        IScheduler _scheduler;
+        IOptionsSnapshot<List<CronMethod>> _cronMethod;
+     
+        public ValuesController(IBackgroundRepository backgroundRepository, IScheduler scheduler, IOptionsSnapshot<List<CronMethod>> cronMethod)
+        {
+            _scheduler = scheduler;
+            _cronMethod = cronMethod;
+            _backgroundRepository = backgroundRepository;
+          
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "value1", "value1" };
+        }
+        [HttpGet("{second}")]
+        public ActionResult Get(int second)
+        {
+            //获取路径方法 
+            var path =Directory.GetCurrentDirectory() + "/appsettings.json";
+            var json = System.IO.File.ReadAllText(path);
+
+            var obj = Newtonsoft.Json.JsonConvert.DeserializeObject<dynamic>(json);
+            obj.CronJob[0].CronExpression = $"0/{second} * * * * ?";
+            _scheduler.Clear().Wait();
+            foreach (var cronJob in obj.CronJob)
+            {              
+                Console.WriteLine($"{cronJob.MethodName},{cronJob.CronExpression}");
+                QuartzServicesUtilities.StartJob<BackgroundJob>(_scheduler, cronJob.CronExpression.ToString(), cronJob.MethodName.ToString());
+            }
+            json = Newtonsoft.Json.JsonConvert.SerializeObject(obj,Newtonsoft.Json.Formatting.Indented);
+            System.IO.File.WriteAllText(path, json);
+            return Ok();
+        }      
+
+    }
+}
---
+++ aspnetcore3.0course/Quartz.NetDemo/QuartzNetDemo01/Startup.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Quartz;
+using QuartzNetDemo01.Model;
+using QuartzNetDemo01.Model.DataModel;
+using IHostingEnvironment = Microsoft.AspNetCore.Hosting.IHostingEnvironment;
+
+namespace QuartzNetDemo01
+{
+    public class Startup
+    {
+  
+        public Startup(IConfiguration configuration)
+        {        
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.Configure<List<CronMethod>>(Configuration.GetSection("CronJob"));
+            services.AddTransient<IBackgroundRepository, BackgroundRepository>();
+
+
+            services.AddQuartz(typeof(BackgroundJob));
+
+            services.AddMvc()
+                .AddNewtonsoftJson();
+
+          
+        }
+
+       
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IScheduler scheduler, IOptionsSnapshot<List<CronMethod>> cronJobs)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            foreach (var cronJob in cronJobs.Value)
+            {
+                QuartzServicesUtilities.StartJob<BackgroundJob>(scheduler, cronJob.CronExpression, cronJob.MethodName);
+            }
+            app.UseRouting(routes =>
+            {
+                routes.MapControllers();
+            });
+
+            app.UseAuthorization();
+        }
+    }
+}
