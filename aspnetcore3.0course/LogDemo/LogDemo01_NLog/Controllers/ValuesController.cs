f99d214d016922b394870e141e9ca7d982b09ed0
桂素伟 <axzxs2001@163.com>
2019-04-16 12:31:43 +0900

    LoggerMessage 的高性能日志记录

--- aspnetcore3.0course/LogDemo/LogDemo01_NLog/Controllers/ValuesController.cs
+++ aspnetcore3.0course/LogDemo/LogDemo01_NLog/Controllers/ValuesController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
 namespace LogDemo01_NLog.Controllers
 {
@@ -10,10 +11,16 @@ namespace LogDemo01_NLog.Controllers
     [ApiController]
     public class ValuesController : ControllerBase
     {
+        readonly ILogger<ValuesController> _logger;
+        public ValuesController(ILogger<ValuesController> logger)
+        {
+            _logger = logger;
+        }
         // GET api/values
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
+            _logger.LogInformation("-----------get-------------");
             return new string[] { "value1", "value2" };
         }
 
---
+++ aspnetcore3.0course/LogDemo/LogDemo03_LoggerMessage/Controllers/ValuesController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace LogDemo03_LoggerMessage.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        readonly ILogger<ValuesController> _logger;
+        public ValuesController(ILogger<ValuesController> logger)
+        {
+            _logger = logger;
+        }
+               
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            _logger.IndexPageRequested("----------get--------------");
+            return new string[] { "value1", "value2" };
+        }
+
+
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            for (int i = 1; i < 5; i++)
+            {
+                _logger.DeleteLog(id+i);
+            }
+            return "value";
+        }     
+    }
+
+    public static class LogExt
+    {
+        public static void IndexPageRequested(this ILogger logger, string message)
+        {
+
+            _indexPageRequested(logger, message, null);
+        }
+        private static readonly Action<ILogger, string, Exception> _indexPageRequested = LoggerMessage.Define<string>(
+                 LogLevel.Information,
+                 new EventId(1),
+                 "自定义日志消息:{0}");
+
+        public static void DeleteLog(this ILogger logger, int id)
+        {
+            DeleteScope(logger, id);
+        }
+        private static Func<ILogger, int, IDisposable> DeleteScope = LoggerMessage.DefineScope<int>("删除了{id}");
+
+    }
+}
