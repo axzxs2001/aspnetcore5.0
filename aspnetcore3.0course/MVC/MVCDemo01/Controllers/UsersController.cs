655b02f6aac90e3390a7041af8e75c8bee48538c
桂素伟 <axzxs2001@163.com>
2019-05-01 15:37:36 +0900

    ViewModel

---
+++ aspnetcore3.0course/MVC/MVCDemo01/Controllers/UsersController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MVCDemo01.Models;
+
+namespace MVCDemo01.Controllers
+{
+    public class UsersController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View(new List<UserViewModel>() { new UserViewModel { UserName="gsw",Password="123456" } });
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(UserViewModel userViewModel)
+        {
+            return View();
+        }
+        public IActionResult Delete()
+        {
+            return View( new UserViewModel { UserName = "gsw", Password = "123456" });
+        }
+        [HttpPost]
+        public IActionResult Delete(UserViewModel userViewModel)
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Edit(UserViewModel userViewModel)
+        {
+            return View();
+        }
+        public IActionResult Edit()
+        {
+             return View(new UserViewModel { UserName = "gsw", Password = "123456" });
+        }
+        public IActionResult Details()
+        {
+            return View(new UserViewModel { UserName = "gsw", Password = "123456" });
+        }
+    }
+}
---
+++ aspnetcore3.0course/MVC/MVCDemo01/Models/UserViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCDemo01.Models
+{
+    public class UserViewModel
+    {
+        [Key]
+        [Required(ErrorMessage = "用户名不能为空")]
+        [Display(Name = "用户名")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "密码不能为空")]
+        [StringLength(8, ErrorMessage = "密码长度在6-8位长度", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "密码")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "确认密码不能为空")]
+        [StringLength(8, ErrorMessage = "密码长度在6-8位长度", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        
+        [Compare("Password",ErrorMessage = "确认密码与密码不一致")]
+        [Display(Name = "确认密码")]
+        public string ConfirmPassword { get; set; }
+
+    }
+}
