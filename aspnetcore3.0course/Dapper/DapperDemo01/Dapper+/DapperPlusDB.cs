ec00397989dcaee9af52217e262ab64857440811
桂素伟 <axzxs2001@163.com>
2019-05-10 10:58:53 +0900

    重构Dapper

--- aspnetcore3.0course/Dapper/DapperDemo01/Dapper+/DapperPlusDB.cs
+++ aspnetcore3.0course/Dapper/DapperDemo01/Dapper+/DapperPlusDB.cs
@@ -1,6 +1,7 @@
 ﻿using Dapper;
 using Microsoft.Extensions.Configuration;
 using Newtonsoft.Json.Linq;
+using Polly;
 using System;
 using System.Collections.Generic;
 using System.Data;
@@ -50,7 +51,12 @@ namespace DapperDemo01
         /// <returns></returns>
         public IEnumerable<T> Query<T>(string sql, object param = null, IDbTransaction transaction = null, bool buffered = false, int? commandTimeout = null, CommandType? commandType = null)
         {
-            return _dbConnection.Query<T>(sql, param, transaction, buffered, commandTimeout, commandType);
+            IEnumerable<T> result = null;
+            PollyInvock(() =>
+            {
+                result= _dbConnection.Query<T>(sql, param, transaction, buffered, commandTimeout, commandType);
+            });
+            return result;
         }
         /// <summary>
         /// 查询异步方法
@@ -63,39 +69,13 @@ namespace DapperDemo01
         /// <param name="commandType">command类型</param>
         public async Task<IEnumerable<T>> QueryAsync<T>(string sql, object param = null, IDbTransaction transaction = null, int? commandTimeout = null, CommandType? commandType = null)
         {
-            return await _dbConnection.QueryAsync<T>(sql, param, transaction, commandTimeout, commandType);
-        }
-
-        /// <summary>
-        /// 查询多返回结果方法
-        /// </summary>
-        /// <typeparam name="T">映射实体类</typeparam>
-        /// <param name="sql">sql语句</param>
-        /// <param name="param">参数对象</param>
-        /// <param name="transaction">事务</param>    
-        /// <param name="commandTimeout">command超时时间(秒)</param>
-        /// <param name="commandType">command类型</param>
-        /// <returns></returns>
-        public GridReader QueryMultiple(string sql, object param = null, IDbTransaction transaction = null, int? commandTimeout = null, CommandType? commandType = null)
-        {
-            return _dbConnection.QueryMultiple(sql, param, transaction, commandTimeout, commandType);
-        }
-
-
-        /// <summary>
-        /// 异常查询多返回结果方法
-        /// </summary>
-        /// <typeparam name="T">映射实体类</typeparam>
-        /// <param name="sql">sql语句</param>
-        /// <param name="param">参数对象</param>
-        /// <param name="transaction">事务</param>    
-        /// <param name="commandTimeout">command超时时间(秒)</param>
-        /// <param name="commandType">command类型</param>
-        /// <returns></returns>
-        public async Task<GridReader> QueryMultipleAsync(string sql, object param = null, IDbTransaction transaction = null, int? commandTimeout = null, CommandType? commandType = null)
-        {
-            return await _dbConnection.QueryMultipleAsync(sql, param, transaction, commandTimeout, commandType);
-        }
+            IEnumerable<T> result = null;
+            await PollyInvockAsync(async () =>
+            {
+                result = await _dbConnection.QueryAsync<T>(sql, param, transaction, commandTimeout, commandType);
+            });
+            return result;
+        }    
 
 
         /// <summary>
@@ -109,7 +89,12 @@ namespace DapperDemo01
         /// <returns></returns>
         public int Execute(string sql, object param = null, IDbTransaction transaction = null, int? commandTimeout = null, CommandType? commandType = null)
         {
-            return _dbConnection.Execute(sql, param, transaction, commandTimeout, commandType);
+            var result = 0;
+            PollyInvock(() =>
+            {
+                result = _dbConnection.Execute(sql, param, transaction, commandTimeout, commandType);
+            });
+            return result;
         }
 
         /// <summary>
@@ -123,71 +108,14 @@ namespace DapperDemo01
         /// <returns></returns>
         public async Task<int> ExecuteAsync(string sql, object param = null, IDbTransaction transaction = null, int? commandTimeout = null, CommandType? commandType = null)
         {
-            return await _dbConnection.ExecuteAsync(sql, param, transaction, commandTimeout, commandType);
-        }
-
-        /// <summary>
-        /// 执行动态json参数的增删改语句
-        /// </summary>
-        /// <param name="sql">sql语句</param>
-        /// <param name="jsonString">json参数</param>
-        /// <param name="timeStampField">时间戳字段</param>
-        /// <param name="transaction">事务</param>
-        /// <param name="commandTimeout">command超时时间(秒)</param>
-        /// <param name="commandType">command类型</param>
-        /// <returns></returns>
-        public int Execute(string sql, string jsonString, string timeStampField = "timestamp", IDbTransaction transaction = null, int? commandTimeout = null, CommandType? commandType = null)
-        {
-            var pars = JsonToEntity(jsonString, timeStampField);
-            return _dbConnection.Execute(sql, pars, transaction, commandTimeout, commandType);
-        }
-
-        /// <summary>
-        /// 异常执行动态json参数的增删改语句
-        /// </summary>
-        /// <param name="sql">sql语句</param>
-        /// <param name="jsonString">json参数</param>
-        /// <param name="timeStampField">时间戳字段</param>
-        /// <param name="transaction">事务</param>
-        /// <param name="commandTimeout">command超时时间(秒)</param>
-        /// <param name="commandType">command类型</param>
-        /// <returns></returns>
-        public async Task<int> ExecuteAsync(string sql, string jsonString, string timeStampField = "timestamp", IDbTransaction transaction = null, int? commandTimeout = null, CommandType? commandType = null)
-        {
-            var pars = JsonToEntity(jsonString, timeStampField);
-            return await _dbConnection.ExecuteAsync(sql, pars, transaction, commandTimeout, commandType);
-        }
-        /// <summary>
-        /// json转DynamicParameters
-        /// </summary>
-        /// <param name="jsonString">json字符串</param>
-        /// <param name="timeStampField">时间戳字段</param>
-        /// <returns></returns>
-        private DynamicParameters JsonToEntity(string jsonString, string timeStampField)
-        {
-            var jsonObj = Newtonsoft.Json.JsonConvert.DeserializeObject<dynamic>(jsonString);
-            var pars = new DynamicParameters();
-            foreach (var jToken in (jsonObj as JObject).Children())
+            var result = 0;
+            await PollyInvockAsync(async () =>
             {
-                //只处理json属性
-                if (jToken.Type == JTokenType.Property)
-                {
-                    //处理时间戳字段的转换
-                    if (((jToken as JProperty).Value as JValue).Path == timeStampField)
-                    {
-                        pars.Add((jToken as JProperty).Value.Path, (byte[])(jToken as JProperty).Value);
-                    }
-                    else
-                    {
-                        var jsonPro = ((jToken as JProperty).Value as JValue);
-                        pars.Add(jsonPro.Path, jsonPro.Value);
-                    }
-                }
-            }
-            return pars;
+                result = await _dbConnection.ExecuteAsync(sql, param, transaction, commandTimeout, commandType);
+            });
+            return result;
         }
-
-
+         
 
         /// <summary>
         /// 查询单值
@@ -201,7 +129,12 @@ namespace DapperDemo01
         /// <returns></returns>
         public T ExecuteScalar<T>(string sql, object param = null, IDbTransaction transaction = null, int? commandTimeout = null, CommandType? commandType = null)
         {
-            return _dbConnection.ExecuteScalar<T>(sql, param, transaction, commandTimeout, commandType);
+            var result = default(T);
+            PollyInvock(() =>
+            {
+                result =  _dbConnection.ExecuteScalar<T>(sql, param, transaction, commandTimeout, commandType);
+            });
+            return result;
         }
 
         /// <summary>
@@ -216,59 +149,47 @@ namespace DapperDemo01
         /// <returns></returns>
         public async Task<T> ExecuteScalarAsync<T>(string sql, object param = null, IDbTransaction transaction = null, int? commandTimeout = null, CommandType? commandType = null)
         {
-            return await _dbConnection.ExecuteScalarAsync<T>(sql, param, transaction, commandTimeout, commandType);
+            var result = default(T);
+            await PollyInvockAsync(async () =>
+            {
+                result = await _dbConnection.ExecuteScalarAsync<T>(sql, param, transaction, commandTimeout, commandType);
+            });
+            return result;
         }
 
 
-
-        public IEnumerable<TReturn> Query<TFirst, TSecond, TThird, TFourth, TFifth, TSixth, TSeventh, TReturn>(string sql, Func<TFirst, TSecond, TThird, TFourth, TFifth, TSixth, TSeventh, TReturn> map, object param = null, IDbTransaction transaction = null, bool buffered = true, string splitOn = "Id", int? commandTimeout = null, CommandType? commandType = null)
+        /// <summary>
+        /// 重试调用
+        /// </summary>
+        /// <param name="action"></param>
+        private void PollyInvock(Action action)
         {
-            return _dbConnection.Query<TFirst, TSecond, TThird, TFourth, TFifth, TSixth, TSeventh, TReturn>(sql, map, param, transaction, buffered, splitOn, commandTimeout, commandType);
+            var policy = Policy
+                .Handle<Exception>()            
+                .Retry(3, (excetpion, index, context) =>
+                {                   
+                });
+            policy.Execute(() =>
+            {
+                action();
+            });
         }
-
-        public IEnumerable<TReturn> Query<TFirst, TSecond, TThird, TFourth, TFifth, TSixth, TReturn>(string sql, Func<TFirst, TSecond, TThird, TFourth, TFifth, TSixth, TReturn> map, object param = null, IDbTransaction transaction = null, bool buffered = true, string splitOn = "Id", int? commandTimeout = null, CommandType? commandType = null)
+        /// <summary>
+        /// 异常重试调用
+        /// </summary>
+        /// <param name="func"></param>
+        /// <returns></returns>
+        private async Task PollyInvockAsync(Func<Task> func)
         {
-            return _dbConnection.Query<TFirst, TSecond, TThird, TFourth, TFifth, TSixth, TReturn>(sql, map, param, transaction, buffered, splitOn, commandTimeout, commandType);
-        }
-
-        public IEnumerable<TReturn> Query<TFirst, TSecond, TThird, TFourth, TFifth, TReturn>(string sql, Func<TFirst, TSecond, TThird, TFourth, TFifth, TReturn> map, object param = null, IDbTransaction transaction = null, bool buffered = true, string splitOn = "Id", int? commandTimeout = null, CommandType? commandType = null)
-        {
-            return _dbConnection.Query<TFirst, TSecond, TThird, TFourth, TFifth, TReturn>(sql, map, param, transaction, buffered, splitOn, commandTimeout, commandType);
-        }
-
-        public IEnumerable<TReturn> Query<TFirst, TSecond, TThird, TFourth, TReturn>(string sql, Func<TFirst, TSecond, TThird, TFourth, TReturn> map, object param = null, IDbTransaction transaction = null, bool buffered = true, string splitOn = "Id", int? commandTimeout = null, CommandType? commandType = null)
-        {
-            return _dbConnection.Query<TFirst, TSecond, TThird, TFourth, TReturn>(sql, map, param, transaction, buffered, splitOn, commandTimeout, commandType);
-        }
-
-        public IEnumerable<TReturn> Query<TFirst, TSecond, TThird, TReturn>(string sql, Func<TFirst, TSecond, TThird, TReturn> map, object param = null, IDbTransaction transaction = null, bool buffered = true, string splitOn = "Id", int? commandTimeout = null, CommandType? commandType = null)
-        {
-            return _dbConnection.Query<TFirst, TSecond, TThird, TReturn>(sql, map, param, transaction, buffered, splitOn, commandTimeout, commandType);
-        }
-
-        public IEnumerable<dynamic> Query(string sql, object param = null, IDbTransaction transaction = null, bool buffered = true, int? commandTimeout = null, CommandType? commandType = null)
-        {
-            return _dbConnection.Query(sql, param, transaction, buffered, commandTimeout, commandType);
-        }
-
-        public IEnumerable<TReturn> Query<TFirst, TSecond, TReturn>(string sql, Func<TFirst, TSecond, TReturn> map, object param = null, IDbTransaction transaction = null, bool buffered = true, string splitOn = "Id", int? commandTimeout = null, CommandType? commandType = null)
-        {
-            return _dbConnection.Query<TFirst, TSecond, TReturn>(sql, map, param, transaction, buffered, splitOn, commandTimeout, commandType);
-        }
-
-        public IEnumerable<object> Query(Type type, string sql, object param = null, IDbTransaction transaction = null, bool buffered = true, int? commandTimeout = null, CommandType? commandType = null)
-        {
-            return _dbConnection.Query(type, sql, param, transaction, buffered, commandTimeout, commandType);
-        }
-
-        public IEnumerable<TReturn> Query<TReturn>(string sql, Type[] types, Func<object[], TReturn> map, object param = null, IDbTransaction transaction = null, bool buffered = true, string splitOn = "Id", int? commandTimeout = null, CommandType? commandType = null)
-        {
-            return _dbConnection.Query<TReturn>(sql, types, map, param, transaction, buffered, splitOn, commandTimeout, commandType);
-        }
-
-        public IEnumerable<T> Query<T>(CommandDefinition command)
-        {
-            return _dbConnection.Query<T>(command);
+            var policy = Policy
+                .Handle<Exception>()               
+                .RetryAsync(3, (excetpion, index, context) =>
+                { });
+            await policy.ExecuteAsync(() =>
+            {
+                return func();
+            });
+            //return Task.CompletedTask;
         }
 
         public void Dispose()
