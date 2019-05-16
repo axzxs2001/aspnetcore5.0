5dc375a60c33ad4c44e1d00bd4f2183927f6a924
桂素伟 <axzxs2001@163.com>
2019-05-16 16:34:55 +0900

    写配置文件的案例

--- aspnetcore3.0course/配置文件/ConfigrationDemo/Controllers/ValuesController.cs
+++ aspnetcore3.0course/配置文件/ConfigrationDemo/Controllers/ValuesController.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
 
 namespace ConfigrationDemo.Controllers
 {
@@ -16,19 +18,36 @@ namespace ConfigrationDemo.Controllers
         {
             _appsetting = optionsSnapshot.Value;
         }
-       
-       [HttpGet]
+
+        [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
             return new string[] { "value1", $"Key:{ _appsetting.Key},Name:{_appsetting.Name}" };
-          
+
         }
 
-        // GET api/values/5
+
         [HttpGet("{id}")]
         public ActionResult<string> Get(int id)
         {
-            return "value";
+            //写配置文件的不要
+            if (id == 1)
+            {
+                var jsonFile = Path.Combine(Directory.GetCurrentDirectory(), "Data.json");
+                var jsonContext = System.IO.File.ReadAllText(jsonFile);
+                var data = JsonConvert.DeserializeObject<dynamic>(jsonContext);
+                return Convert.ToString(data.Data1);
+            }
+            else
+            {
+                var jsonFile = Path.Combine(Directory.GetCurrentDirectory(), "Data.json");
+                var jsonContext = System.IO.File.ReadAllText(jsonFile);
+                var data = JsonConvert.DeserializeObject<dynamic>(jsonContext);
+                data.Data1 = data.Data1 + DateTime.Now.ToString();
+                jsonContext = JsonConvert.SerializeObject(data);
+                System.IO.File.WriteAllText(jsonFile, jsonContext);
+                return "成功写入：" + Convert.ToString(data.Data1);
+            }
         }
 
         // POST api/values
