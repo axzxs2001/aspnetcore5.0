0c3f18802bfe5818f4bdfc06b45981421591c96c
桂素伟 <axzxs2001@163.com>
2019-05-17 14:59:52 +0900

    添加单点登录案例

---
+++ aspnetcore3.0course/验证和授权/SSO/Project02/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Project02.Models;
+
+namespace Project02.Controllers
+{
+    [AutoValidateAntiforgeryToken]
+    [Authorize(Roles = "admin,system")]
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+        [AllowAnonymous]
+        [HttpGet("login")]
+        public IActionResult Login()
+        {
+            return Redirect("http://localhost:5400/login");
+        }
+    }
+}
