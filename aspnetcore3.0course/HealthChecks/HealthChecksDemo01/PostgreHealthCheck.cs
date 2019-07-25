27ea8a67bead3d1b76815610cde54c2a0ff11a56
桂素伟 <axzxs2001@163.com>
2019-07-25 12:46:31 +0900

    健康检查Demo

---
+++ aspnetcore3.0course/HealthChecks/HealthChecksDemo01/PostgreHealthCheck.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HealthChecksDemo01
+{ 
+    /// <summary>
+    /// postgre健康检查
+    /// </summary>
+    public class PostgreHealthCheck : IHealthCheck
+    {
+        private readonly IDbConnection _dbConnection;
+        private readonly IConfiguration _configuration;
+
+        public PostgreHealthCheck(IDbConnection dbConnection, IConfiguration configuration)
+        {
+            _dbConnection = dbConnection;
+            _configuration = configuration;
+        }
+
+        public Task<HealthCheckResult> CheckHealthAsync(
+            HealthCheckContext context,
+            CancellationToken cancellationToken = default(CancellationToken))
+        {
+
+            var healthCheckResultHealthy = true;
+            using (_dbConnection)
+            {
+                _dbConnection.ConnectionString = _configuration.GetConnectionString("Postgre");
+                try
+                {
+                    _dbConnection.Open();
+                }
+                catch
+                {
+                    healthCheckResultHealthy = false;
+                }
+                if (healthCheckResultHealthy)
+                {
+                    return Task.FromResult(
+                        HealthCheckResult.Healthy("健康"));
+                }
+                return Task.FromResult(
+                    HealthCheckResult.Unhealthy("不健康"));
+            }
+        }
+    }
+}
--- aspnetcore3.0course/HealthChecks/HealthChecksDemo01/Startup.cs
+++ aspnetcore3.0course/HealthChecks/HealthChecksDemo01/Startup.cs
@@ -1,12 +1,15 @@
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
@@ -21,16 +24,37 @@ namespace HealthChecksDemo01
 
         public IConfiguration Configuration { get; }
 
-    
+
         public void ConfigureServices(IServiceCollection services)
         {
+
+            //最简单模式
             services.AddHealthChecks();
+            //自定义健康检查
+            services.AddScoped<IDbConnection, Npgsql.NpgsqlConnection>();
+            services.AddHealthChecks()
+                .AddCheck<PostgreHealthCheck>("postgre_health_check");
+
+
+            //services.AddHealthChecks()
+            //    .AddCheck("postgre_health_check", () =>
+            //    HealthCheckResult.Healthy("postgre_health_check is ok!"), tags: new[] { "postgre" });
+
+
             services.AddControllers();
         }
 
-     
+
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            //最简单模式
+            app.UseHealthChecks("/health");
+
+            //加端口
+            app.UseHealthChecks("/health", 8800);
+
+
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
