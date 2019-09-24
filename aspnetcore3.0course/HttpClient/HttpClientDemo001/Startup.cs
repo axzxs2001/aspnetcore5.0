9906cfb460ebcff14da9501bde1f91b85fbb6da0
桂素伟 <axzxs2001@163.com>
2019-09-24 11:17:55 +0900

    需要证书的HttpClient

--- aspnetcore3.0course/HttpClient/HttpClientDemo001/Startup.cs
+++ aspnetcore3.0course/HttpClient/HttpClientDemo001/Startup.cs
@@ -1,6 +1,10 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net.Http;
+using System.Security.Authentication;
+using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
 using HttpClientDemo001.Repository;
 using Microsoft.AspNetCore.Builder;
@@ -28,7 +32,7 @@ namespace HttpClientDemo001
         public void ConfigureServices(IServiceCollection services)
         {
             //基本用法
-             services.AddHttpClient();
+            services.AddHttpClient();
             //命名客户端
             services.AddHttpClient("nameclient5000", c =>
             {
@@ -44,8 +48,50 @@ namespace HttpClientDemo001
             //重试
             services.AddHttpClient("pollyclient5000")
                 .AddTransientHttpErrorPolicy(p => p.WaitAndRetryAsync(3, _ => TimeSpan.FromSeconds(1)))
-                .AddTransientHttpErrorPolicy(p => p.CircuitBreakerAsync(15, TimeSpan.FromSeconds(30))); 
-
+                .AddTransientHttpErrorPolicy(p => p.CircuitBreakerAsync(15, TimeSpan.FromSeconds(30)));
+          
+            //证书Client
+            services.AddHttpClient("CertificateClient").ConfigurePrimaryHttpMessageHandler(provider =>
+            {
+                var handler = new HttpClientHandler();
+                handler.ClientCertificateOptions = ClientCertificateOption.Manual;
+                handler.SslProtocols = SslProtocols.Tls12 | SslProtocols.Tls | SslProtocols.None | SslProtocols.Tls11;
+                try
+                {
+                    var crt = new X509Certificate2(Directory.GetCurrentDirectory() + "/client.pfx", "cccccc");
+                    handler.ClientCertificates.Add(crt);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+                //验证服务器证书是否正规
+                handler.ServerCertificateCustomValidationCallback = (message, cer, chain, errors) =>
+                {
+                    //var verify = false;
+                    //foreach (X509ChainElement element in chain.ChainElements)
+                    //{
+                    //    Console.WriteLine("SerialNumber:{0}", element.Certificate.SerialNumber);
+                    //    Console.WriteLine("Element subject name: {0}", element.Certificate.Subject);
+                    //    Console.WriteLine("Element issuer name: {0}", element.Certificate.Issuer);
+                    //    Console.WriteLine("Element certificate valid until: {0}", element.Certificate.NotAfter);
+                    //    Console.WriteLine("Element certificate is valid: {0}", element.Certificate.Verify());
+                    //    Console.WriteLine("Element error status length: {0}", element.ChainElementStatus.Length);
+                    //    Console.WriteLine("Element information: {0}", element.Information);
+                    //    Console.WriteLine("Number of element extensions: {0}{1}", element.Certificate.Extensions.Count, Environment.NewLine);
+                    //    if (element.Certificate.Issuer == cer.Issuer)
+                    //    {
+                    //        verify = element.Certificate.Verify();
+                    //    }
+                    //}
+                    //Console.WriteLine($"*********   X509Certificate2.Verify={cer.Verify()}");
+                    //Console.WriteLine($"*********   Element certificate is valid: {verify}");
+                    var res = chain.Build(cer);
+                    Console.WriteLine($"*********   X509Chain.Build={res}");
+                    return res;
+                };
+                return handler;
+            });
 
 
             services.AddControllers()
